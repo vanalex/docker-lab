d098b57d7cbb88772bed1608049629250878b62f
alejandropajaronjorquera <a.pajaron.jorquera@dkbcodefactory.com>
2021-07-20 08:52:53 +0200

    add t

---
+++ terraform/apache-web-server/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+  shared_credentials_file = var.creds
+  profile = "default"
+}
+
+resource "aws_instance" "app" {
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-2a"
+  ami               = "ami-0c55b159cbfafe1f0"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo service apache2 start
+              EOF
+}
---
+++ terraform/apache-web-server/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  default = "us-west-2"
+}
+variable "instance_type" {}
+variable "creds" {}
